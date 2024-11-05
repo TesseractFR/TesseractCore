e8939c919983bd22bffd09723e9a81e582de8216
Gabriel Rayzal <gabriel.rayzal@epita.fr>
2024-11-05 20:14:31 +0100

    :construction: init project

---
+++ src/main/kotlin/onl/tesseract/core/TesseractCorePlugin.kt
@@ -0,0 +1,14 @@
+package onl.tesseract.core
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TesseractCorePlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
