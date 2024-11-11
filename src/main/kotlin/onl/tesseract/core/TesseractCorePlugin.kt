27cb9071cb1251b5c6687b95d3f0382e00017a2a
Gabriel Rayzal <gabriel.rayzal@epita.fr>
2024-11-11 15:43:14 +0100

    build v4: Register default staff titles

--- src/main/kotlin/onl/tesseract/core/TesseractCorePlugin.kt
+++ src/main/kotlin/onl/tesseract/core/TesseractCorePlugin.kt
@@ -2,6 +2,9 @@ package onl.tesseract.core
 
 import onl.tesseract.core.achievement.AchievementService
 import onl.tesseract.core.persistence.hibernate.achievement.AchievementHibernateRepository
+import onl.tesseract.core.persistence.hibernate.title.TitleHibernateRepository
+import onl.tesseract.core.title.StaffTitle
+import onl.tesseract.core.title.TitleService
 import onl.tesseract.lib.service.ServiceContainer
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -13,6 +16,11 @@ class TesseractCorePlugin : JavaPlugin() {
         ServiceContainer.getInstance().registerService(
             AchievementService::class.java, AchievementService(AchievementHibernateRepository)
         )
+        val titleService = ServiceContainer.getInstance().registerService(
+            TitleService::class.java, TitleService(TitleHibernateRepository)
+        )
+
+        StaffTitle.entries.forEach { titleService.save(it.title) }
     }
 
     override fun onDisable() {
