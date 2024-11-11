593747f90910ad9096f71f1b4f56a5847c4648fb
Gabriel Rayzal <gabriel.rayzal@epita.fr>
2024-11-11 17:39:57 +0100

    build v4: Fix achievements

--- src/main/kotlin/onl/tesseract/core/achievement/AchievementRepository.kt
+++ src/main/kotlin/onl/tesseract/core/achievement/AchievementRepository.kt
@@ -1,10 +1,15 @@
 package onl.tesseract.core.achievement
 
 import onl.tesseract.lib.repository.Repository
+import java.util.UUID
 
 interface AchievementRepository : Repository<Achievement, Int> {
 
     fun getByName(name: String): Achievement?
 
     fun getAll(): List<Achievement>
+
+    fun getAchievements(player: UUID): Collection<Achievement>
+
+    fun addAchievementToPlayer(player: UUID, achievement: Achievement)
 }
--- src/main/kotlin/onl/tesseract/core/persistence/hibernate/achievement/AchievementRepository.kt
+++ src/main/kotlin/onl/tesseract/core/persistence/hibernate/achievement/AchievementRepository.kt
@@ -3,6 +3,7 @@ package onl.tesseract.core.persistence.hibernate.achievement
 import onl.tesseract.core.achievement.Achievement
 import onl.tesseract.core.achievement.AchievementRepository
 import onl.tesseract.core.persistence.hibernate.DaoUtils
+import java.util.UUID
 
 object AchievementHibernateRepository : AchievementRepository {
 
@@ -15,7 +16,7 @@ object AchievementHibernateRepository : AchievementRepository {
 
     override fun getByName(name: String): Achievement? {
         DaoUtils.executeInsideTransaction { session ->
-            val query = session.createQuery("FROM Achievement WHERE name = :name", AchievementEntity::class.java)
+            val query = session.createQuery("FROM AchievementEntity WHERE name = :name", AchievementEntity::class.java)
             query.setParameter("name", name)
             return query.uniqueResult().toModel()
         }
@@ -30,10 +31,32 @@ object AchievementHibernateRepository : AchievementRepository {
 
     override fun getAll(): List<Achievement> {
         DaoUtils.executeInsideTransaction { session ->
-            val query = session.createQuery("SELECT Achievement a FROM Achievement", AchievementEntity::class.java)
+            val query =
+                session.createQuery("SELECT AchievementEntity a FROM AchievementEntity ", AchievementEntity::class.java)
             return query.resultList.map { it.toModel() }
         }
         return listOf()
+    }
+
+    override fun getAchievements(player: UUID): Collection<Achievement> {
+        DaoUtils.executeInsideTransaction { session ->
+            val query = session.createQuery(
+                "SELECT AchievementEntity FROM PlayerAchievementsEntity p JOIN AchievementEntity a ON p.achievementID = a.id AND p.player = :player",
+                AchievementEntity::class.java
+            )
+            query.setParameter("player", player)
+            return query.resultList.map { it.toModel() }
+        }
+        return emptyList()
+    }
+
+    override fun addAchievementToPlayer(
+        player: UUID,
+        achievement: Achievement
+    ) {
+        DaoUtils.executeInsideTransaction { session ->
+            session.persist(PlayerAchievementsEntity(player, achievement.id))
+        }
     }
 
     override fun idOf(entity: Achievement): Int = entity.id
