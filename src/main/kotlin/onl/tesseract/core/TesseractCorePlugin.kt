7efc9a35a1b2f40b630546cf63325c23911e227f
Gabriel Rayzal <gabriel.rayzal@epita.fr>
2024-11-12 20:11:36 +0100

    build v4: Boutique menu, trails and filters

--- src/main/kotlin/onl/tesseract/core/TesseractCorePlugin.kt
+++ src/main/kotlin/onl/tesseract/core/TesseractCorePlugin.kt
@@ -2,7 +2,10 @@ package onl.tesseract.core
 
 import onl.tesseract.core.achievement.AchievementService
 import onl.tesseract.core.afk.AfkManager
+import onl.tesseract.core.boutique.BoutiqueService
+import onl.tesseract.core.cosmetics.TrailsAndFilterEventHandlers
 import onl.tesseract.core.persistence.hibernate.achievement.AchievementHibernateRepository
+import onl.tesseract.core.persistence.hibernate.boutique.BoutiqueHibernateRepository
 import onl.tesseract.core.persistence.hibernate.title.TitleHibernateRepository
 import onl.tesseract.core.title.StaffTitle
 import onl.tesseract.core.title.TitleService
@@ -13,6 +16,7 @@ class TesseractCorePlugin : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        instance = this
 
         ServiceContainer.getInstance().registerService(
             AchievementService::class.java, AchievementService(AchievementHibernateRepository)
@@ -20,13 +24,22 @@ class TesseractCorePlugin : JavaPlugin() {
         val titleService = ServiceContainer.getInstance().registerService(
             TitleService::class.java, TitleService(TitleHibernateRepository)
         )
-
         StaffTitle.entries.forEach { titleService.save(it.title) }
+        ServiceContainer.getInstance().registerService(
+            BoutiqueService::class.java, BoutiqueService(
+                BoutiqueHibernateRepository
+            )
+        )
 
         this.server.pluginManager.registerEvents(AfkManager.getINSTANCE(), this)
+        this.server.pluginManager.registerEvents(TrailsAndFilterEventHandlers(), this)
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+    }
+
+    companion object {
+        lateinit var instance: TesseractCorePlugin
     }
 }
---
+++ src/main/kotlin/onl/tesseract/core/cosmetics/TrailsAndFilterEventHandlers.kt
@@ -0,0 +1,81 @@
+package onl.tesseract.core.cosmetics
+
+import com.destroystokyo.paper.ParticleBuilder
+import onl.tesseract.core.TesseractCorePlugin
+import onl.tesseract.core.boutique.BoutiqueService
+import onl.tesseract.lib.service.ServiceContainer
+import onl.tesseract.tesseractlib.cosmetics.ElytraTrails
+import onl.tesseract.tesseractlib.cosmetics.FlyFilter
+import onl.tesseract.tesseractlib.util.Util
+import org.bukkit.Color
+import org.bukkit.Particle
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityToggleGlideEvent
+import org.bukkit.event.player.PlayerToggleFlightEvent
+import kotlin.math.cos
+import kotlin.math.sin
+
+class TrailsAndFilterEventHandlers : Listener {
+
+    @EventHandler
+    fun onGlide(event: EntityToggleGlideEvent) {
+        if (event.entityType != EntityType.PLAYER) return
+
+        val boutiqueService = ServiceContainer[BoutiqueService::class.java]
+        val player = event.entity as Player
+
+        val playerInfo = boutiqueService.getPlayerBoutiqueInfo(player.uniqueId)
+        TesseractCorePlugin.instance.server.scheduler.runTaskTimer(TesseractCorePlugin.instance, { task ->
+            if (!player.isOnline || !player.isGliding)
+                task.cancel()
+            val trail = playerInfo.activeTrail
+            if (trail == ElytraTrails.NONE) return@runTaskTimer
+            val builder = ParticleBuilder(trail.getParticle())
+            if (trail == ElytraTrails.SHINNING)
+                builder.count(1)
+            else
+                builder.count(2)
+            builder.offset(.5, .5, .5)
+            if (trail == ElytraTrails.POTION || trail == ElytraTrails.MUSICAL)
+                builder.extra(0.2)
+            else
+                builder.extra(0.0)
+            builder.location(Util.Locations.backward(event.getEntity().location, 2.0))
+            builder.receivers(100)
+            if (trail == ElytraTrails.REDSTONE)
+                builder.color(Color.RED)
+            builder.spawn()
+        }, 0, 2)
+    }
+
+    @EventHandler
+    fun onFly(event: PlayerToggleFlightEvent) {
+        val player = event.player
+        val boutiqueService = ServiceContainer[BoutiqueService::class.java]
+        val playerInfo = boutiqueService.getPlayerBoutiqueInfo(player.uniqueId)
+        TesseractCorePlugin.instance.server.scheduler.runTaskTimer(TesseractCorePlugin.instance, { task ->
+            if (!player.isFlying) task.cancel()
+            val selectedFilter = playerInfo.activeFlyFilter
+            if (selectedFilter == FlyFilter.NONE) return@runTaskTimer
+            var loc = player.location
+            loc = loc.add((-cos(loc.yaw)).toDouble(), 0.0, -sin(loc.yaw.toDouble()))
+            val particle = selectedFilter.getParticle()
+            val extra: Double = if (event.getPlayer().isSprinting) 0.5 else 0.0
+            if (selectedFilter == FlyFilter.REDSTONE)
+                loc.getWorld()
+                    .spawnParticle(
+                        particle, loc, 15, .5, .5, .5, .5, Particle.DustOptions(
+                            Color.RED,
+                            1.2f
+                        )
+                    )
+            else if (selectedFilter == FlyFilter.POTION || selectedFilter == FlyFilter.MUSICAL)
+                loc.getWorld().spawnParticle(particle, loc, 15, .5, .5, .5, 0.2);
+            else
+                loc.getWorld().spawnParticle(particle, loc, 15, .5, .5, .5, extra);
+        }, 0, 10)
+    }
+}
